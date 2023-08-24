b9789c232abcf204471f4b4cf73bf3c6b053f5e2
Belle Hollander <bellehollander@gmail.com>
2023-08-24 14:59:28 -0400

    found each invoiceLine item and the name of the track that was purchases

---
+++ line_item_track.sql
@@ -0,0 +1,7 @@
+# Provide a query that shows each Invoice line item, with the name of the track that was purchased.
+
+
+SELECT il.*, t.Name
+FROM InvoiceLine il
+JOIN Track t ON il.TrackId = t.TrackId
+ORDER BY il.InvoiceLineId
